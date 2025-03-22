48551d54b387a65d6a4800764823a75ac8d6eaaf
gokul2178 <165740161+gokul2178@users.noreply.github.com>
2025-03-21 18:51:55 -0700

    better directory management

--- Main.py
+++ Main.py
@@ -11,12 +11,20 @@ directory = os.path.dirname(filePath)
 os.chdir(directory)
 
 
+if os.path.exists:
+    shutil.rmtree("Data")
+    os.mkdir("Data")
+else: 
+    os.mkdir("Data")
+
+#======================================================
 
 def main():
     print("")
+
     generateToken()
     
-    risingPosts(subreddit = "Scams", numPosts = 5, numComments = 5)
+    risingPosts(subreddit = "Scams", numPosts = 3, numComments = 3)
 
     convertCSV()
 
